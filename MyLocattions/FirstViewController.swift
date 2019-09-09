52ada7cce1226cbecec1bbca04cc55fd87e9c3c2
Rafael Lehn <rbatista@webeleven.com.br>
2019-09-09 11:44:33 -0300

    Initial Commit

---
+++ MyLocattions/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocattions
+//
+//  Created by Webeleven on 09/09/19.
+//  Copyright © 2019 Rafael Lehn. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyLocattions/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocattions
+//
+//  Created by Webeleven on 09/09/19.
+//  Copyright © 2019 Rafael Lehn. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
